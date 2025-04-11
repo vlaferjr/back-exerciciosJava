a4ddb410629599ec08cb6dd746cb41a321c45e59
Vlad <119672242+vlaferjr@users.noreply.github.com>
2025-04-11 07:54:50 -0300

    continuação dos exercícios para explorar as funcionalidades do stream

--- src/lambdas_streams_frameworks/generics/stream_estatisticas/Principal.java
+++ src/lambdas_streams_frameworks/generics/stream_estatisticas/Principal.java
@@ -34,7 +34,73 @@ public class Principal {
         System.out.println(palavrasAgrupadas);
 
 
+        /*3 - Dada a lista de nomes abaixo, concatene-os separados por vírgula.
+        No código a seguir, há um exemplo prático do resultado esperado.*/
+        List<String> nomes = Arrays.asList("Alice", "Bob", "Charlie");
+        System.out.println("\n-------------------------NOMES SEPARADOS POR VÍRGULA ----------------------------");
+        // Resultado Esperado: "Alice, Bob, Charlie"
 
+        List<String> nomesSeparados = nomes.stream()
+                //colocando um espaço entre os nomes
+                .map(nome -> nome + "")
+                //coletando numa nova lista
+                .collect(Collectors.toList());
+        System.out.println(nomesSeparados);
+
+        /*---- Resposta do instrutor*/
+
+        String result = nomes.stream()
+                .collect(Collectors.joining(", "));
+        System.out.println(result);
+
+        /*4 - Dada a lista de números inteiros abaixo, calcule a soma dos quadrados dos números pares.*/
+        System.out.println("\n----------------Soma dos quqadrados dos pares--------------");
+        List<Integer> listaNumeros = Arrays.asList(1,2,3,4,5,6,7,8,9);
+        IntSummaryStatistics resultado = listaNumeros.stream()
+                //filtrando os numeros pares
+                .filter(numero -> numero % 2 == 0)
+                //transformando o número em seu quadrado
+                .map(numero -> (numero * numero))
+                //verificando se a lista está saindo com o valor ao quadrado
+                .peek(numero -> System.out.println(numero))
+                //convertendo inteiro para inteiro
+                .mapToInt(Integer::intValue)
+                //fechando a summaryStatistics
+                .summaryStatistics();
+        //imprimindo a soma dos valores dos quadrados
+        System.out.println("Soma dos quadrados dos pares: " + resultado.getSum());
+
+        /*Resposta do instrutor*/
+        int somaQuadrados = listaNumeros.stream()
+                .filter(numero -> numero % 2 == 0)
+                .map(numero -> (numero * numero))
+                .reduce(0, Integer::sum);
+
+        System.out.println(somaQuadrados);
+
+        /*5 - Dada uma lista de números inteiros, separe os números pares dos ímpares.*/
+        System.out.println("\n-------------------------Separação de Numeros pares e ímpares ----------------------------");
+        List<Integer> listaNs = Arrays.asList(1,2,3,4,5,6,7,8,9);
+        /*System.out.println("Números pares: ");
+        listaNs.stream()
+                .filter(numero -> numero % 2 == 0)
+                .forEach(numero -> System.out.println(numero));
+
+        System.out.println("Números ímpares: ");
+        listaNs.stream()
+                .filter(numero -> !(numero % 2 == 0))
+                .forEach(numero -> System.out.println(numero));*/
+
+        /*Resposta do instrutor*/
+
+        //transformando para boolean a lista de inteiros
+        Map<Boolean, List<Integer>> listaSeparada = listaNs.stream()
+                //particionando a lista em números pares e ímpares
+                .collect(Collectors.partitioningBy(numero -> numero % 2 == 0));
+        //se a resposta for true, imprime os pares
+        System.out.println("Numeros pares: " + listaSeparada.get(true));
+        //se a resposta for false, imprime os ímpares
+        System.out.println("Numeros ímpares: " + listaSeparada.get(false));
     }
 
 }

f4568570ca073c43ab1f1136a4b535c94d85c89d
Vlad <119672242+vlaferjr@users.noreply.github.com>
2025-04-21 10:41:46 -0300

    Inicio do projeto VeiculosFipe

---
+++ src/revisao_2025/VeiculosFipe/src/main/java/br/com/alura/VeiculosFipe/model/MarcasVeiculos.java
@@ -0,0 +1,10 @@
+package br.com.alura.VeiculosFipe.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record MarcasVeiculos(@JsonProperty("codigo") int codigo,
+                             @JsonProperty("nome") String nome){
+
+        }

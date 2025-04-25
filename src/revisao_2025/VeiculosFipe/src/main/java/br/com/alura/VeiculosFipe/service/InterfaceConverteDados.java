8ba822bd6ca2a6cad5880d45b5b0d0f14b8cfd1b
Vlad <119672242+vlaferjr@users.noreply.github.com>
2025-04-25 08:12:51 -0300

    Delete src/revisao_2025/VeiculosFipe directory

--- src/revisao_2025/VeiculosFipe/src/main/java/br/com/alura/VeiculosFipe/service/InterfaceConverteDados.java
+++
@@ -1,9 +0,0 @@
-package br.com.alura.VeiculosFipe.service;
-
-import java.util.List;
-
-/*Interface genérica ConverteDados que vem do json para uma classe genérica*/
-public interface InterfaceConverteDados {
-    <T> List<T> obterDados (String json, Class<T> classe);
-}
-

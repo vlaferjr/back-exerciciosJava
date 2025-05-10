160efca161d661ad99954d2c1e3a541a98037cce
Vlad <119672242+vlaferjr@users.noreply.github.com>
2025-05-10 10:08:18 -0300

    Delete src/revisao_2025/VeiculosFipe-VProfessora directory
    exclusão do VeiculoFipe-VProfessora

--- src/revisao_2025/VeiculosFipe-VProfessora/src/main/java/br/com/alura/VeiculosFipe_VProfessora/principal/Principal.java
+++
@@ -1,4 +0,0 @@
-package br.com.alura.VeiculosFipe_VProfessora.principal;
-
-public class Principal {
-}
--- src/revisao_2025/VeiculosFipe-VProfessora/src/main/java/br/com/alura/VeiculosFipe_VProfessora/service/IConverteDados.java
+++
@@ -1,6 +0,0 @@
-package br.com.alura.VeiculosFipe_VProfessora.service;
-
-public interface IConverteDados {
-    //converter dados para uma classe
-    <T> T obterDados (String json, Class<T>classe);
-}
